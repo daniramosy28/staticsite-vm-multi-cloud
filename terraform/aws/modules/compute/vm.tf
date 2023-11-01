c0ba3e716317ea9d0d1a7ff387376acfb6aafe0c
Daniele Ramos <danieleramos@example.com>
2023-11-01 20:32:01 -0300

    ALteração

--- terraform/aws/modules/compute/vm.tf
+++ terraform/aws/modules/compute/vm.tf
@@ -31,13 +31,13 @@ resource "aws_security_group" "sg_public" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    #ingress {
-     #   description = "TCP/80 from All"
-      #  from_port   = 443
-       # to_port     = 443
-        #protocol    = "tcp"
-        #cidr_blocks = ["0.0.0.0/0"]
-   # }
+    ingress {
+        description = "TCP/80 from All"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
 
 }
