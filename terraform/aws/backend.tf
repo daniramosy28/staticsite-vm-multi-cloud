4dd260053fe967eade9f954f055a21e07ebbaf15
Daniele Ramos <danieleramos@example.com>
2023-11-01 20:02:38 -0300

    Subindo

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -1,8 +1,8 @@
 terraform {
       backend "s3" {
-    bucket         = "tf-state-multi-dani"
+    bucket         = "tf-state-multi-daniele"
     key            = "path/to/terraform.tfstate"
-    dynamodb_table = "tf-state-dani-multi"
+    dynamodb_table = "tf-state-daniele-multi"
     region         = "us-east-1"
   }
 }
