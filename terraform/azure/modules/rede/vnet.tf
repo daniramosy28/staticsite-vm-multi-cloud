f0994e5da3ecf6497f719c5489b21534afd8b377
Daniele Ramos <danieleramos@example.com>
2023-11-01 20:17:50 -0300

    Subindo

--- terraform/azure/modules/rede/vnet.tf
+++ terraform/azure/modules/rede/vnet.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "staticsite-vm-vnet"
+  name                = "staticsite-vm-vnet-dani"
   location            = "${var.location}"
   resource_group_name = "${var.rg_name}"
   address_space       = ["${var.vnet_cidr}"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "staticsite-vm-subnet"
+  name                 = "staticsite-vm-subnet-dani"
   resource_group_name  = "${var.rg_name}"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["${var.subnet_cidr}"]
